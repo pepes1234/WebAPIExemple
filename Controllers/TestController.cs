f9128bbdaa4aa29c599bc26510d43b1f85e6a29e
pepes1234 <103346831+pepes1234@users.noreply.github.com>
2022-08-10 17:25:24 -0300

    Adição de Controladores.

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -5,8 +5,15 @@ using Microsoft.AspNetCore.Mvc;
 public class TestController : ControllerBase
 {
     [HttpGet]
+    //rota, metodo, head, body são os 4 cavalero do apocalpse
     public string QlquerNome()
     {
         return "My App is Working!";
+    }
+    [HttpGet("quadratica/{x}")]
+    public string Get(int x, int a = 1, int b = 0, int c = 0)
+    {
+        int resultado = (a * x * x + b * x + c);
+        return resultado.ToString();
     }
 }
