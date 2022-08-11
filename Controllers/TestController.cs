39b03ed21b5446813b514dc426589af21b8641e3
pepes1234 <103346831+pepes1234@users.noreply.github.com>
2022-08-11 13:55:17 -0300

    Adição de controlador para baskhara

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -5,15 +5,46 @@ using Microsoft.AspNetCore.Mvc;
 public class TestController : ControllerBase
 {
     [HttpGet]
-    //rota, metodo, head, body são os 4 cavalero do apocalpse
-    public string QlquerNome()
+    public string Get()
     {
         return "My App is Working!";
     }
+
     [HttpGet("quadratica/{x}")]
     public string Get(int x, int a = 1, int b = 0, int c = 0)
     {
-        int resultado = (a * x * x + b * x + c);
+        int resultado = a * x * x + b * x + c;
         return resultado.ToString();
+    }
+
+    [HttpGet("solucao")]
+    public object Get(
+        double a = 1, double b = 0, double c = 0)
+    {
+        if (a == 0)
+        {
+            return new {
+                status = "Fail",
+                message = "'a' não pode ser zero."
+            };
+        }   
+
+        double delta = b * b - 4 * a * c;
+        if (delta < 0)
+        {
+            return new {
+                status = "Fail",
+                message = "delta não pode ser negativo"
+            };
+        }
+
+        double x1 = (-b + Math.Sqrt(delta)) / a;
+        double x2 = (-b - Math.Sqrt(delta)) / a;
+
+        return new {
+            status = "Sucesso",
+            solucao1 = x1,
+            solucao2 = x2
+        };
     }
 }
