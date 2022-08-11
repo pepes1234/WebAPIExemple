7d0874a7f1d785b248bd785572aa49ccfb5e5b45
pepes1234 <lucas.pepes07@gmail.com>
2022-08-11 18:55:33 -0300

    Criação de projetos base para ações com CPF

---
+++ Services/CpfService.cs
@@ -0,0 +1,24 @@
+public class CpfService
+{
+    public CpfService(int seed)
+    {
+        this.rand = new Random(seed);
+    }
+    Random rand;
+
+    public bool Validate(string cpf)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Generate()
+    {
+        throw new NotImplementedException();
+    }
+
+    // detalhes em: https://www.calculadorafacil.com.br/computacao/validar-cpf
+    private string getValidationDigits(string cpf9digits)
+    {
+        
+    }
+}
