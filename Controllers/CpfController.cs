5f9420b8d04bf66e1456c585a8182cef49a9b5a3
pepes1234 <lucas.pepes07@gmail.com>
2022-08-16 21:49:54 -0300

    Implementação de verificações completas para erros

--- Controllers/CpfController.cs
+++ Controllers/CpfController.cs
@@ -7,19 +7,57 @@ public class CpfController : ControllerBase
     [HttpGet("validate/{cpf}")]
     public object Validate([FromServices]CpfService cpfService, string cpf)
     {
+        
         cpf = cpf.Replace(".", string.Empty).Replace("-", string.Empty).Replace("/", string.Empty)
         .Replace(" ", string.Empty).Replace(",", string.Empty).Replace(";", string.Empty).Replace("=", string.Empty)
         .Replace("|", string.Empty);
+
+        
+        long cpflong;
+        if(long.TryParse(cpf, out cpflong) == false)
+        {
+            return new{
+                status = "Fail",
+                message = "Cpf não pode conter caracteres especiais ou letras."
+            };
+        }
+        if(cpf.Length < 11)
+        {
+            return new{
+                status = "Fail",
+                message = "Cpf com quantidade de digitos insuficientes"
+            };
+        }
+
         if(cpf.Length > 11)
         {
             return new{
                 status = "Fail",
-            }
+                message = "Cpf com quantidade de digitos excedentes"
+            };
         }
-        return new {
-            status = cpfService.Validate(cpf),
 
+        
+        if(cpfService.Validate(cpf) == true)
+        {
+            return new {
+                status = "Sucess",
+                message = "Cpf Valido"
+            };  
+        }
+
+        if(cpfService.Validate(cpf) == false)
+        {
+            return new {
+                status = "Fail",
+                message = "Cpf Invalido"
+            };  
+        }
+
+        return new {
+            status = "fail"
         };
+        
     }
 
     [HttpGet("generate")]
